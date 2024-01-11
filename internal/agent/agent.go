ed24ac9d6e9ad0ef4dbe1da52943ef3542bfbb12
Viacheslav Starostin <viacheslavstarostin1@yandex.kz>
2024-01-12 01:45:54 +0600

    Update agent

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -49,16 +49,16 @@ func SendMetrics(
 
 			mu.Lock()
 			gw = pool.Get().(*gzip.Writer)
-			l.Info().Msgf("gw points to: %p", &gw)
+			l.Info().Msgf("gw points to: %p", gw)
 			buf.Reset()
 			gw.Reset(buf)
-			l.Info().Msgf("buffer points to: %p", &buf)
-			l.Info().Msgf("buffer's content: %v", buf.String())
+			l.Info().Msgf("buffer points to: %p", buf)
+			l.Info().Msgf("buffer's content: %v", (*buf).String())
 			n, err := gw.Write(b)
 			if err != nil {
 				return
 			}
-			l.Info().Msgf("buffer's content: %v", buf.String())
+			l.Info().Msgf("buffer's content: %v", (*buf).String())
 			gw.Close()
 			pool.Put(gw)
 
