f5ab9449c538ece09af922d2ca3528f73e07fb9c
Viacheslav Starostin <viacheslavstarostin1@yandex.kz>
2024-07-01 01:15:12 +0500

    Handle JSON config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"os"
 
 	"github.com/caarlos0/env/v6"
 )
@@ -17,34 +19,26 @@ type Config struct {
 	Key             string `env:"KEY"`
 	RateLimit       int    `env:"RATE_LIMIT"`
 	CryptoKey       string `env:"CRYPTO_KEY"`
+	JSONConfigPath  string `env:"CONFIG"`
 }
 
 func NewAgent() (Config, error) {
 	flags := parseAgentFlags()
 
+	if flags.JSONConfigPath != "" {
+		jsonConfig, err := parseJSONConfig(flags.JSONConfigPath)
+		if err != nil {
+			return Config{}, err
+		}
+		mergeConfigs(&flags, &jsonConfig)
+	}
+
 	config := Config{}
 	if err := env.Parse(&config); err != nil {
 		return Config{}, err
 	}
-
-	if config.ServerAddress == "" {
-		config.ServerAddress = flags.ServerAddress
-	}
-	if config.ReportInterval == 0 {
-		config.ReportInterval = flags.ReportInterval
-	}
-	if config.PollInterval == 0 {
-		config.PollInterval = flags.PollInterval
-	}
-	if config.Key == "" {
-		config.Key = flags.Key
-	}
-	if config.RateLimit == 0 {
-		config.RateLimit = flags.RateLimit
-	}
-	if config.CryptoKey == "" {
-		config.CryptoKey = flags.CryptoKey
-	}
+	mergeConfigs(&config, &flags)
+	setDefaultValues(&config)
 
 	return config, nil
 }
@@ -52,35 +46,19 @@ func NewAgent() (Config, error) {
 func NewServer() (Config, error) {
 	flags := parseServerFlags()
 
+	if flags.JSONConfigPath != "" {
+		jsonConfig, err := parseJSONConfig(flags.JSONConfigPath)
+		if err != nil {
+			return Config{}, err
+		}
+		mergeConfigs(&flags, &jsonConfig)
+	}
+
 	config := Config{}
 	if err := env.Parse(&config); err != nil {
 		return Config{}, err
 	}
-
-	if config.ServerAddress == "" {
-		config.ServerAddress = flags.ServerAddress
-	}
-	if config.Restore == nil {
-		config.Restore = flags.Restore
-	}
-	if config.StoreInterval == nil {
-		config.StoreInterval = flags.StoreInterval
-	}
-	if config.FileStoragePath == "" {
-		config.FileStoragePath = flags.FileStoragePath
-	}
-	if config.DatabaseDNS == "" {
-		config.DatabaseDNS = flags.DatabaseDNS
-	}
-	if config.Key == "" {
-		config.Key = flags.Key
-	}
-	if config.RateLimit == 0 {
-		config.RateLimit = flags.RateLimit
-	}
-	if config.CryptoKey == "" {
-		config.CryptoKey = flags.CryptoKey
-	}
+	mergeConfigs(&config, &flags)
 
 	if config.DatabaseDNS != "" {
 		config.FileStoragePath = ""
@@ -92,12 +70,13 @@ func NewServer() (Config, error) {
 }
 
 func parseAgentFlags() Config {
-	serverAddress := flag.String("a", "localhost:8080", "HTTP server endpoint address")
-	reportInterval := flag.Int("r", 10, "report interval to the server (in seconds)")
-	pollInterval := flag.Int("p", 2, "interval to gather metrics (in seconds)")
+	serverAddress := flag.String("a", "", "HTTP server endpoint address")
+	reportInterval := flag.Int("r", 0, "report interval to the server (in seconds)")
+	pollInterval := flag.Int("p", 0, "interval to gather metrics (in seconds)")
 	key := flag.String("k", "", "key")
-	rateLimit := flag.Int("l", 1, "rate limit")
+	rateLimit := flag.Int("l", 0, "rate limit")
 	cryptoKey := flag.String("crypto-key", "", "Path to the public key")
+	cfg := flag.String("config", "", "Path to JSON config file")
 	flag.Parse()
 
 	return Config{
@@ -107,17 +86,19 @@ func parseAgentFlags() Config {
 		Key:            *key,
 		RateLimit:      *rateLimit,
 		CryptoKey:      *cryptoKey,
+		JSONConfigPath: *cfg,
 	}
 }
 
 func parseServerFlags() Config {
-	serverAddress := flag.String("a", "localhost:8080", "address and port to run server")
-	fileStoragePath := flag.String("f", "/tmp/metrics-db.json", "file storage path")
+	serverAddress := flag.String("a", "", "address and port to run server")
+	fileStoragePath := flag.String("f", "", "file storage path")
 	databaseDSN := flag.String("d", "", "database DSN")
-	restore := flag.Bool("r", true, "restore")
-	storeInterval := flag.Int("i", 300, "interval")
+	restore := flag.Bool("r", false, "restore")
+	storeInterval := flag.Int("i", 0, "interval")
 	key := flag.String("k", "", "")
 	cryptoKey := flag.String("crypto-key", "", "Path to the private key")
+	cfg := flag.String("config", "", "Path to JSON config file")
 	flag.Parse()
 
 	return Config{
@@ -128,5 +109,78 @@ func parseServerFlags() Config {
 		StoreInterval:   storeInterval,
 		Key:             *key,
 		CryptoKey:       *cryptoKey,
+		JSONConfigPath:  *cfg,
+	}
+}
+
+func parseJSONConfig(filepath string) (Config, error) {
+	f, err := os.ReadFile(filepath)
+	if err != nil {
+		return Config{}, err
+	}
+	var c Config
+	if err := json.Unmarshal(f, &c); err != nil {
+		return Config{}, err
+	}
+
+	return c, nil
+}
+
+func mergeConfigs(target, source *Config) {
+	if target.ServerAddress == "" && source.ServerAddress != "" {
+		target.ServerAddress = source.ServerAddress
+	}
+	if target.ReportInterval == 0 && source.ReportInterval != 0 {
+		target.ReportInterval = source.ReportInterval
+	}
+	if target.PollInterval == 0 && source.PollInterval != 0 {
+		target.PollInterval = source.PollInterval
+	}
+	if target.FileStoragePath == "" && source.FileStoragePath != "" {
+		target.FileStoragePath = source.FileStoragePath
+	}
+	if target.Restore == nil && source.Restore != nil {
+		target.Restore = source.Restore
+	}
+	if target.StoreInterval == nil && source.StoreInterval != nil {
+		target.StoreInterval = source.StoreInterval
+	}
+	if target.DatabaseDNS == "" && source.DatabaseDNS != "" {
+		target.DatabaseDNS = source.DatabaseDNS
+	}
+	if target.Key == "" && source.Key != "" {
+		target.Key = source.Key
+	}
+	if target.RateLimit == 0 && source.RateLimit != 0 {
+		target.RateLimit = source.RateLimit
+	}
+	if target.CryptoKey == "" && source.CryptoKey != "" {
+		target.CryptoKey = source.CryptoKey
+	}
+}
+
+func setDefaultValues(config *Config) {
+	if config.ServerAddress == "" {
+		config.ServerAddress = "localhost:8080"
+	}
+	if config.ReportInterval == 0 {
+		config.ReportInterval = 10
+	}
+	if config.PollInterval == 0 {
+		config.PollInterval = 2
+	}
+	if config.FileStoragePath == "" {
+		config.FileStoragePath = "/tmp/metrics-db.json"
+	}
+	if config.Restore == nil {
+		restore := true
+		config.Restore = &restore
+	}
+	if config.StoreInterval == nil {
+		storeInterval := 300
+		config.StoreInterval = &storeInterval
+	}
+	if config.RateLimit == 0 {
+		config.RateLimit = 1
 	}
 }
